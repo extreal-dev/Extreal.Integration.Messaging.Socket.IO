ce5742a0441b075f8a16a21a430f49ad40dda99e
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2024-02-26 12:39:18 +0900

    Use Deno.Serve

--- MessagingServer~/index.ts
+++ MessagingServer~/index.ts
@@ -1,4 +1,3 @@
-import { serve } from "https://deno.land/std@0.212.0/http/server.ts";
 import { Server, Socket } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
 
 const appPort = 3030;
@@ -103,4 +102,4 @@ io.on("connection", async (socket: Socket) => {
     log(() => `client connected: socket id=${socket.id}`);
 });
 log(() => "=================================Restarted======================================");
-await serve(io.handler(), { port: appPort, });
+await Deno.serve({ port: appPort, }, io.handler());
